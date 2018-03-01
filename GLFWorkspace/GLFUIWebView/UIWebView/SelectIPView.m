a845e34f1a0572793caf451cfe84c8d9ebe1e39e
GLFguolongfei <15618632831@163.com>
2018-03-01 14:24:28 +0800

    发送

--- GLFWorkspace/GLFUIWebView/UIWebView/SelectIPView.m
+++ GLFWorkspace/GLFUIWebView/UIWebView/SelectIPView.m
@@ -25,6 +25,8 @@
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
+    self.layer.cornerRadius = 10;
+    self.layer.masksToBounds = YES;
     [self setupData];
     [self setupUI];
 }
