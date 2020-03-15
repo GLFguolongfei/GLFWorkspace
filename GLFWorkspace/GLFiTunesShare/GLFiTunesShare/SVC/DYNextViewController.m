fe2adc4792c0e8950aa4c9b051d93eaa5374829d
longfeifengwu <15618632831@163.com>
2020-03-15 21:11:07 +0800

    fs

--- GLFWorkspace/GLFiTunesShare/GLFiTunesShare/SVC/DYNextViewController.m
+++ GLFWorkspace/GLFiTunesShare/GLFiTunesShare/SVC/DYNextViewController.m
@@ -309,8 +309,8 @@
     [userDefaults synchronize];
 
     [self showHUD];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self showStringHUD:@"清空完成" second:1.5];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self showStringHUD:@"清空完成" second:2];
     });
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(6 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
