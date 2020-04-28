3f73ef4b250392dba69e80f1b9e35c970196af04
longfeifengwu <15618632831@163.com>
2020-04-28 16:34:19 +0800

    fs

--- GLFWorkspace/GLFiTunesShare/GLFiTunesShare/SVC/DYNextViewController.m
+++ GLFWorkspace/GLFiTunesShare/GLFiTunesShare/SVC/DYNextViewController.m
@@ -277,7 +277,6 @@
     [alertVC addAction:cancelAction];
     
     UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"选择播放" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        [self hiddenNaviBar];
         SelectItemView *selectView = [[SelectItemView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth/4*3, kScreenHeight/4*3)];
         selectView.parentVC = self;
         selectView.pageType = 2;
@@ -287,6 +286,7 @@
         [self lew_presentPopupView:selectView animation:[LewPopupViewAnimationSpring new] dismissed:^{
             NSLog(@"动画结束");
         }];
+        [self hiddenNaviBar];
     }];
     [alertVC addAction:okAction];
     
